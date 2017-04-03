800a86fd0d1dce15ccb7485964f8512b6d555e01
Sir_Josh <josh.moodley09@gmail.com>
2017-04-03 12:07:48 +0200

    Updated GIS package

---
+++ MinDeployment/GISModule/src/main/java/org/gis/DB_Functions.java
@@ -0,0 +1,240 @@
+package org.gis;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Created by Josh on 2017/04/02.
+ */
+public class DB_Functions
+{
+//    CREATE TABLE
+
+    public void CreateTable(String tableName)
+    {
+        Connection c = null;
+        Statement stmt = null;
+
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:10000/navup", "postgres", "password");
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE " + tableName +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " AGE            INT     NOT NULL, " +
+                    " ADDRESS        CHAR(50), " +
+                    " SALARY         REAL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        }
+        catch ( Exception e )
+        {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table created successfully");
+    }
+
+
+//    INSERT INTO DATABASE
+
+    public void Insert()
+    {
+        Connection c = null;
+        Statement stmt = null;
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:10000/napup","postgres", "password");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            stmt.close();
+            c.commit();
+            c.close();
+        }
+        catch (Exception e)
+        {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
+    }
+
+
+//    SEARCH
+    public void SearchByBuildingName(String buildingName)
+    {
+//        Gson jsonObj = new Gson();
+//
+//        String jsonString = jsonObj.toJson();
+
+        Connection c = null;
+        Statement stmt = null;
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:10000/napup","postgres", "password");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+            while ( rs.next() )
+            {
+                int id = rs.getInt("id");
+                String  name = rs.getString("name");
+                int age  = rs.getInt("age");
+                String  address = rs.getString("address");
+                float salary = rs.getFloat("salary");
+                System.out.println( "ID = " + id );
+                System.out.println( "NAME = " + name );
+                System.out.println( "AGE = " + age );
+                System.out.println( "ADDRESS = " + address );
+                System.out.println( "SALARY = " + salary + "\n ");
+            }
+
+            rs.close();
+            stmt.close();
+            c.close();
+
+        }
+        catch ( Exception e )
+        {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+    }
+
+    public void SearchByCoordinates(double latitude, double longitue)
+    {
+
+    }
+
+    public void SearchByRoomNumber(String room)
+    {
+//        Make sure format is e.g "IT 4-1" or "EMB 3-125"
+    }
+
+//     UPDATE
+    public void UpdateBuilding(String buildingName)
+    {
+        Connection c = null;
+        Statement stmt = null;
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:10000/navup","postgres", "password");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "UPDATE COMPANY set SALARY = 25000.00 where ID=1;";
+            stmt.executeUpdate(sql);
+            c.commit();
+
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+            while ( rs.next() )
+            {
+                int id = rs.getInt("id");
+                String  name = rs.getString("name");
+                int age  = rs.getInt("age");
+                String  address = rs.getString("address");
+                float salary = rs.getFloat("salary");
+                System.out.println( "ID = " + id );
+                System.out.println( "NAME = " + name );
+                System.out.println( "AGE = " + age );
+                System.out.println( "ADDRESS = " + address );
+                System.out.println( "SALARY = " + salary + "\n");
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        }
+        catch ( Exception e )
+        {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+    }
+
+    public void UpdateBuildingRoom(String buildingName, String buildingRoom)
+    {
+
+    }
+
+    public void UpdateBuildingCoordinates(double latitude, double logitude)
+    {
+
+    }
+
+    public void UpdateBuildingRoomCoordinates(double latitude, double lonitude)
+    {
+
+    }
+
+//    DELETE
+    public void DeleteBuilding()
+    {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/testdb",
+                            "manisha", "123");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "DELETE from COMPANY where ID=2;";
+            stmt.executeUpdate(sql);
+            c.commit();
+
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+            while ( rs.next() ) {
+                int id = rs.getInt("id");
+                String  name = rs.getString("name");
+                int age  = rs.getInt("age");
+                String  address = rs.getString("address");
+                float salary = rs.getFloat("salary");
+                System.out.println( "ID = " + id );
+                System.out.println( "NAME = " + name );
+                System.out.println( "AGE = " + age );
+                System.out.println( "ADDRESS = " + address );
+                System.out.println( "SALARY = " + salary );
+                System.out.println();
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Operation done successfully");
+    }
+
+
+    public void DeleteBuildingRoom()
+    {
+
+    }
+
+}
+
---
+++ MinDeployment/GISModule/src/main/java/org/gis/PostgreSQLJDBC.java
@@ -0,0 +1,26 @@
+package org.gis;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PostgreSQLJDBC
+{
+    public static void main(String args[])
+    {
+        Connection c = null;
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:10000/navup", "postgres", "password");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+
+
+    }
+}
